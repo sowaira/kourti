fe77cc8b31da12d01e2ddfe094f8b9c48043b632
saraka <sara.slila7@gmail.com>
2016-01-24 14:39:29 +0100

    chnage production.rb for heroku prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,83 +1,50 @@
 # encoding: utf-8
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+  if ENV['NO_CACHE_CLASS'].present?
+    config.cache_classes = false
+  else
+    config.cache_classes = true
+  end
 
-  # Code is not reloaded between requests.
-  config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
+  config.consider_all_requests_local = false
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  # config.cache_store = :dalli_store, (ENV["MEMCACHIER_SERVERS"] || "").split(","),
+  #                   {:username => ENV["MEMCACHIER_USERNAME"],
+  #                    :password => ENV["MEMCACHIER_PASSWORD"],
+  #                    :pool_size => 3,
+  #                    :failover => true,
+  #                    :socket_timeout => 1.5,
+  #                    :compress => true,
+  #                    :socket_failure_delay => 0.2
+  #                   }
+
   config.action_controller.perform_caching = true
-
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
-
-  # Disable Rails's static asset server (Apache or nginx will already do this).
+  config.static_cache_control = "public, max-age=30000000"
   config.serve_static_assets = true
-
-  # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
-  # Generate digests for assets URLs.
   config.assets.digest = true
-
-  # `config.assets.precompile` has moved to config/initializers/assets.rb
-
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
-
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
-
-  # Set to :debug to see everything in the log.
-  config.log_level = :info
-
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
-
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
-
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
+  config.assets.version = '1.0'
+  config.log_level = :debug
   config.i18n.fallbacks = true
-
-  # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+
+  domain = "kourti.herokuapp.com"
+
+  config.action_mailer.default_url_options = { host: domain }
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_APIKEY'],
+    authentication: "login",
+    domain: domain
+  }
 end
